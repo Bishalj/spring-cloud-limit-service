13d57df9ba555847f1e0a50e52d96cd875a67f50
bishal.jaiswal <bishal.jaiswal@paytminsurance.co.in>
2020-10-20 22:29:05 +0530

    Initial commit

---
+++ src/test/java/com/bishal/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishal.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
